a0605faa156e422b1908d19f2c2e4daebc37860e
Andrew Boyd <andrew@hireboyd.com>
2014-09-28 12:32:59 -0400

    Remove unused nodes from scene when the exit bounds

--- Space Cannon/GameScene.swift
+++ Space Cannon/GameScene.swift
@@ -11,8 +11,18 @@ import SpriteKit
 let _mainLayer = SKNode()
 let _cannon = SKSpriteNode(imageNamed: "Cannon")
 
+let SHOOT_SPEED = 400.0
+
+func radiansToVector(radians:CGFloat) -> CGVector {
+	var vector = CGVector(CGFloat(cosf(Float(radians))), CGFloat(sinf(Float(radians))))
+	return vector
+}
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
+		
+		// Disable gravity
+		self.physicsWorld.gravity = CGVectorMake(0.0, 0.0)
 		
 		// Add background
 		let background = SKSpriteNode(imageNamed: "Starfield")
@@ -40,16 +50,42 @@ class GameScene: SKScene {
 		
 		_cannon.runAction(SKAction.repeatActionForever(rotateCannon))
     }
+	
+	func shoot() {
+		let ball = SKSpriteNode(imageNamed: "Ball")
+		ball.name = "ball"
+		let rotationVector = radiansToVector(_cannon.zRotation)
+		
+		ball.position = CGPointMake(_cannon.position.x + (_cannon.size.width/2.5 * rotationVector.dx),
+									_cannon.position.y + (_cannon.size.width/2.5 * rotationVector.dy))
+		ball.physicsBody = SKPhysicsBody(circleOfRadius: 6.0)
+		
+		let x_speed = CGFloat(Double(rotationVector.dx) * SHOOT_SPEED)
+		let y_speed = CGFloat(Double(rotationVector.dy) * SHOOT_SPEED)
+		
+		ball.physicsBody?.velocity = CGVectorMake(x_speed, y_speed)
+		
+		_mainLayer.addChild(ball)
+	}
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
 			
-			
+			shoot()
 			
 		}
     }
+	
+	override func didSimulatePhysics() {
+		_mainLayer.enumerateChildNodesWithName("ball") {
+			node, stop in
+			if ( !CGRectContainsPoint(self.frame, node.position) ) {
+				node.removeFromParent()
+			}
+		}
+	}
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
